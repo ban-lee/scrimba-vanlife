f7dc1fd48c4c6c7909847721f2af2d237d87db2e
Ban Lee <53801519+ban-lee@users.noreply.github.com>
2023-05-03 11:37:14 -0400

    Add ability to filter by van type

--- src/components/chip/chip.jsx
+++ src/components/chip/chip.jsx
@@ -18,12 +18,13 @@ export function Chip(props) {
 
     if (props.isSelected) classes.push(styles.selected);
     if (props.isSmall) classes.push(styles['chip--small']);
+    if (props.onClick) classes.push(styles['chip--clickable']);
 
     return classes.join(' ');
   }
 
   return (
-    <div className={getClasses()}>
+    <div className={getClasses()} onClick={props.onClick}>
       {VAN_TYPES[props.type]?.label}
     </div>
   );
--- src/pages/vans/vans.jsx
+++ src/pages/vans/vans.jsx
@@ -1,9 +1,13 @@
 import styles from './vans.module.css';
 import { Van } from '/src/components/van';
+import { Chip, VAN_TYPES } from '/src/components/chip';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 export function Vans() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [vans, setVans] = useState([]);
+  const typeFilter = searchParams.get('type');
 
   useEffect(() => {
     fetch('/api/vans')
@@ -13,11 +17,45 @@ export function Vans() {
       });
   }, []);
 
+  function handleClick(type) {
+    setSearchParams((searchParams) => {
+      if (type === null || searchParams.get('type') === type) {
+        searchParams.delete('type');
+      } else {
+        searchParams.set('type', type);
+      }
+
+      return searchParams;
+    });
+  }
+
   return (
     <div className={styles.content}>
       <h1 className="h1">Explore our van options</h1>
+      <div className={styles.filters}>
+        {Object.keys(VAN_TYPES).map((type) => {
+          return (
+            <Chip
+              key={type}
+              type={type}
+              isSelected={typeFilter === type}
+              onClick={() => handleClick(type)}
+            />
+          );
+        })}
+        {!!typeFilter && (
+          <button
+            className={`link ${styles.clearBtn}`}
+            onClick={() => handleClick(null)}
+          >
+            Clear filters
+          </button>
+        )}
+      </div>
       <div className={styles.vans}>
-        {vans.map((van) => <Van key={van.id} van={van} />)}
+        {vans
+          .filter((van) => !!typeFilter ? van.type === typeFilter : true)
+          .map((van) => <Van key={van.id} van={van} />)}
       </div>
     </div>
   );
